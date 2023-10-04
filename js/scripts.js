17ce478c7c073517818b38cf036853040e642475
z631200 <z631200@gmail.com>
2023-10-04 23:54:55 +0800

    bg speed

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,5 @@
-const API_KEY='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'
+// 获取视频元素
+var video = document.querySelector(".bg-video");
 
-const form = document.getElementById('chat-form');
-const myText =  document.getElementById('mytext');
-const responseText = document.getElementById('response');
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const mytext = myText.value.trim();
-    
-    if (mytext){
-        try{
-            const response = await fetch('https://api.openai.com/v1/chat/completions',{
-                method:'POST',
-                headers:{
-                    'Content-Type':'application/json',
-                    'Authorization':`Bearer ${API_KEY}`,
-                },
-                body:JSON.stringify({
-                    model:'gpt-4',
-                    message: [{role:'user', content:mytext}],
-                    tempature:1.0,
-                    top_p:1.0,
-                    n:1,
-                    stream:false,
-                    presence_penalty:0.0,
-                    frequency_penalty:0.0,
-                }),
-            });
-
-            if(response.ok){
-                const data = await response.json();
-                responseTextarea.value = data.choices[0].message.content;
-            }
-            else{
-                responseTextarea.vaule =  'Error: unable to connect to API' 
-            }
-            print("success");
-        }catch(err){
-            console.error(err);
-            responseTextarea.value = 'Error: something went wrong';
-        }
-    }
-});
+// 设置播放速度
+video.playbackRate = 0.6;
