4495dcb4ebc7c00572e95c46f6b9aa83d23939fb
Skywalker <imsho0218@gmail.com>
2023-10-05 13:36:51 +0800

    fix button switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 const onoffswitch = document.getElementById('myonoffswitch');
 const gcseSearch = document.getElementsByClassName('google_search');
+const chatSearch = document.getElementsByClassName('row input-group-newsletter')
 
 onoffswitch.addEventListener('change', function() {
   if (this.checked) {
     gcseSearch[0].style.display = "block";
+    chatSearch[0].style.display = "none";
     console.log("checked");
   } else {
     gcseSearch[0].style.display = "none";
+    chatSearch[0].style.display = "block";
     console.log("unchecked");
   }
 });
