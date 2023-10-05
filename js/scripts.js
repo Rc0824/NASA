2aaf50dde47b7b64bb1499247556f7d61a8b5caf
Skywalker <imsho0218@gmail.com>
2023-10-05 13:54:05 +0800

    fix search box

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 const onoffswitch = document.getElementById('myonoffswitch');
 const gcseSearch = document.getElementsByClassName('google_search');
-const chatSearch = document.getElementsByClassName('row input-group-newsletter')
+const chatSearchBox = document.getElementsByClassName('col')
+const chatSearchButton = document.getElementsByClassName('col-auto')
 
 onoffswitch.addEventListener('change', function() {
   if (this.checked) {
     gcseSearch[0].style.display = "block";
-    chatSearch[0].style.display = "none";
+    chatSearchBox[0].style.display = "none";
+    chatSearchButton[0].style.display = "none";
     console.log("checked");
   } else {
     gcseSearch[0].style.display = "none";
-    chatSearch[0].style.display = "block";
+    chatSearchBox[0].style.display = "block";
+    chatSearchButton[0].style.display = "block";
     console.log("unchecked");
   }
 });
